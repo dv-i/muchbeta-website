d072bdfb8509b29757b1ca6ba9d1f5dcc7f71560
Jaimish Ashar <jaimish97@gmail.com>
2023-06-30 19:38:31 -0400

    Removes lorem ipsum on landing page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -89,11 +89,11 @@ function JoinSection(): JSX.Element {
 					<p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">
 						Let's Get Started
 					</p>
-					<p className="mt-6 text-lg leading-8 text-teal-200">
+					{/* <p className="mt-6 text-lg leading-8 text-teal-200">
 						Quis tellus eget adipiscing convallis sit sit eget
 						aliquet quis. Suspendisse eget egestas a elementum
 						pulvinar et feugiat blandit at. In mi viverra elit nunc.
-					</p>
+					</p> */}
 				</div>
 				<div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-none">
 					<dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-3">
